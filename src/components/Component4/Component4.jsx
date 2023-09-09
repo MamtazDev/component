c32a8f70425ca34025e2b32203f8cf94437e245d
tonni-akter-dev <tanniakhter123@gmail.com>
2023-09-09 21:25:40 +0600

    djsfyf

--- src/components/Component4/Component4.jsx
+++ src/components/Component4/Component4.jsx
@@ -1,14 +1,13 @@
-
 import React, { useEffect, useState } from "react";
 import "./Component4.css";
-
 export default function Component4() {
   const [open, setOpen] = useState(false);
   const [optionsOpen, setOptionsOpen] = useState(false);
   const [startValue, setStartValue] = useState(10);
   const [endValue, setEndValue] = useState(15);
-  const [selectedOptions, setSelectedOptions] = useState(["Option 1"]);
-  const [options, setOptions] = useState(["Option 2"]);
+  const [selectedOption, setSelectedOption] = useState(null); // Use null to represent no selection
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [options, setOptions] = useState(["Option 1", "Option 2"]);
 
   const handleOpen = () => {
     setOpen(!open);
@@ -17,34 +16,23 @@ export default function Component4() {
   const handleOptionsOpen = () => {
     setOptionsOpen(!optionsOpen);
   };
-
   const handleOptionSelect = (option) => {
     if (selectedOptions.includes(option)) {
-      if (option !== "Option 1") {
-        setSelectedOptions(selectedOptions.filter((item) => item !== option));
-      }
+      setSelectedOptions(selectedOptions.filter((item) => item !== option));
     } else {
       if (selectedOptions.length < 2) {
         setSelectedOptions([...selectedOptions, option]);
       }
     }
+    setSelectedOption(option);
   };
-
-  useEffect(() => {
-    if (selectedOptions.length === 0) {
-      document.title = "Component Title 4";
-    } else {
-      document.title = `Component Title 4 (${selectedOptions.length})`;
-    }
-  }, [selectedOptions]);
-
   return (
     <>
       <div className={`${!open ? "component_four" : "component_full_four"}`}>
         <div onClick={handleOpen} className="heading">
-          <p className="title">
-            Component Title 4
-            {open && (
+           <p className="title">
+            component title 4
+            {selectedOptions && (
               <span className="ml_3">
                 {selectedOptions.length > 0 ? `(${selectedOptions.length})` : ""}
               </span>
@@ -60,57 +48,48 @@ export default function Component4() {
         </div>
         {open && (
           <div>
-            <ul className="ul_design">
-              <li>From </li>
-              <li>
+            <div className="ul_design">
+              <div>From </div>
+              <div>
                 <input
                   type="text"
                   className="text_input"
                   defaultValue={startValue}
                 />
-              </li>
-              <li>to</li>
-              <li>
+              </div>
+              <div>to</div>
+              <div>
                 <input
                   type="text"
                   className="text_input"
                   defaultValue={endValue}
                 />
-              </li>
-              <li style={{ marginRight: "4px" }}>lorem</li>
-              <li>
+              </div>
+              <div style={{ marginRight: "4px" }}>lorem</div>
+              <div>
                 <div onClick={handleOptionsOpen} className="options">
+                  {selectedOption || "Options"}
                   {optionsOpen ? (
-                    <>
-                      Option 1
-                      {optionsOpen ? (
-                        <i className="m-2 fas fa-angle-down rotate"></i>
-                      ) : (
-                        <i className="m-2 fas fa-angle-down"></i>
-                      )}
+                    <i className="m-2 fas fa-angle-down rotate"></i>
+                  ) : (
+                    <i className="m-2 fas fa-angle-down"></i>
+                  )}
+                  {optionsOpen && (
+                    <div>
                       {options.map((option) => (
-                        <option
+                        <div
                           key={option}
-                          className={selectedOptions.includes(option) ? "selected" : ""}
+                          className={selectedOption === option ? "selected" : ""}
                           onClick={() => handleOptionSelect(option)}
                         >
                           {option}
-                        </option>
+                        </div>
                       ))}
-                    </>
-                  ) : (
-                    <p>
-                      Option 1
-                      {optionsOpen ? (
-                        <i className="m-2 fas fa-angle-down rotate"></i>
-                      ) : (
-                        <i className="m-2 fas fa-angle-down"></i>
-                      )}
-                    </p>
+                    </div>
                   )}
                 </div>
-              </li>
-            </ul>
+              </div>
+            </div>
           </div>
         )}
       </div>
